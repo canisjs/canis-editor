764985833cc7bd9dc50f2bf2f68803d2af118aa0
tge <tgeconf@gmail.com>
2020-02-10 10:58:50 +0800

    autocomplete and error msg

--- js/util/status.js
+++ js/util/status.js
@@ -1,20 +1,52 @@
 class Status {
-    constructor() {
-        this._info = { type: 'log', msg: '' };
-        this.consoleContainerId = '';
+    constructor(consoleContainerId, editor) {
+        this._info = { type: 'log', msg: '', errSpec: '' };
+        this.consoleContainerId = consoleContainerId;
+        this.editor = editor;
+        this.charCheckMap = new Map();
+        this.specWithoutSpace = '';
     }
     set info(info) {
         if (typeof info === 'string') {
-            info = { type: 'log', msg: info };
+            info = { type: 'log', msg: info, errSpec: '' };
         }
         this._info = info;
         if (this.consoleContainerId !== '' && document.getElementById(this.consoleContainerId)) {
-            if(info.type === 'clear'){
+            if (this._info.type === 'clear') {
                 document.getElementById(this.consoleContainerId).innerHTML = '';
             }
             let tmpP = document.createElement('p');
-            let msgStr = '[<span class="' + (info.type === 'error' ? 'error-msg' : 'log-msg') + '">' + (info.type === 'error' ? 'ERROR' : 'LOG') + '</span>]: ' + info.msg;
+            let msgStr = '';
+            let errLink = document.createElement('a');
+
+            switch (this._info.type) {
+                case 'error':
+                    //get the line and col number of the error in the editor
+                    let startPosi = [0, 0], endPosi = [0, 0];
+                    console.log('comming errro: ', this._info.errSpec, this.specWithoutSpace, this.specWithoutSpace.indexOf(this._info.errSpec));
+                    if (this.specWithoutSpace.indexOf(this._info.errSpec) >= 0) {
+                        const startIdx = this.specWithoutSpace.indexOf(this._info.errSpec);
+                        const endIdx = startIdx + this._info.errSpec.length - 1;
+                        startPosi = this.charCheckMap.get(startIdx);
+                        endPosi = this.charCheckMap.get(endIdx);
+                        console.log('erro spec: ', this._info.errSpec, this._info.errSpec.length, startPosi, endPosi);
+                        this.editor.setSelection(new monaco.Selection(startPosi[0], startPosi[1], endPosi[0], endPosi[1] + 1));
+                        this.editor.focus();
+                    }
+                    msgStr = '[<span class="error-msg">ERROR</span>]: ' + this._info.msg + ' ';
+                    errLink.className = 'error-link';
+                    errLink.innerHTML = 'at line ' + (startPosi[0] === endPosi[0] ? startPosi[0] : (startPosi[0] + ' to line ' + endPosi[0]));
+                    errLink.onclick = () => {
+                        this.editor.setSelection(new monaco.Selection(startPosi[0], startPosi[1], endPosi[0], endPosi[1] + 1));
+                        this.editor.focus();
+                    }
+                    break;
+                default:
+                    msgStr = '[<span class="log-msg">LOG</span>]: ' + this._info.msg;
+                    break;
+            }
             tmpP.innerHTML = msgStr;
+            tmpP.appendChild(errLink);
             document.getElementById(this.consoleContainerId).appendChild(tmpP);
         }
 
@@ -25,4 +57,4 @@ class Status {
     }
 }
 
-let status = new Status();
+// let status = new Status();
